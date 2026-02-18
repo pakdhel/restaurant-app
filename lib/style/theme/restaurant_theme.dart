76489b5972810d2fb6bf6031814e11137eeddebd
MUHAMMAD FADHEL H <fadhelhayat17@gmail.com>
2026-02-18 22:36:38 +0800

    menambahkan theme untuk button dan textfield

--- lib/style/theme/restaurant_theme.dart
+++ lib/style/theme/restaurant_theme.dart
@@ -9,6 +9,9 @@ class RestaurantTheme {
       brightness: Brightness.light,
       textTheme: _textTheme,
       useMaterial3: true,
+      elevatedButtonTheme: _elevatedButtonTheme,
+      inputDecorationTheme: _inputDecorationTheme,
+      outlinedButtonTheme: _outlinedButtonTheme
     );
   }
 
@@ -38,6 +41,69 @@ class RestaurantTheme {
       labelLarge: RestaurantTextStyles.labelLarge,
       labelMedium: RestaurantTextStyles.labelMedium,
       labelSmall: RestaurantTextStyles.labelSmall,
+    );
+  }
+
+  static InputDecorationTheme get _inputDecorationTheme {
+    return InputDecorationTheme(
+      filled: true,
+      fillColor: Colors.grey.shade100,
+
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: BorderSide.none,
+      ),
+
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: BorderSide(color: Colors.grey.shade300),
+      ),
+
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: BorderSide(
+          color: RestaurantColors.green.color,
+          width: 2,
+        ),
+      ),
+
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: const BorderSide(color: Colors.red),
+      ),
+
+      contentPadding: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 14,
+      ),
+    );
+  }
+
+  static ElevatedButtonThemeData get _elevatedButtonTheme {
+    return ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: RestaurantColors.green.color,
+        foregroundColor: Colors.white,
+        elevation: 0,
+        padding: const EdgeInsets.symmetric(vertical: 14),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        textStyle: RestaurantTextStyles.labelLarge,
+      ),
+    );
+  }
+
+  static OutlinedButtonThemeData get _outlinedButtonTheme {
+    return OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
+        foregroundColor: RestaurantColors.green.color,
+        side: BorderSide(color: RestaurantColors.green.color),
+        padding: const EdgeInsets.symmetric(vertical: 14),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
     );
   }
 }
