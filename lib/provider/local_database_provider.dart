c38edfe605cb823fb8204309e0a5064234d41a8f
MUHAMMAD FADHEL H <fadhelhayat17@gmail.com>
2026-02-23 14:56:37 +0800

    membuat method di local db provider

--- lib/provider/local_database_provider.dart
+++ lib/provider/local_database_provider.dart
@@ -13,14 +13,50 @@ class LocalDatabaseProvider extends ChangeNotifier {
   String _message = "";
   String get message => _message;
 
+  Restaurant? _restaurant;
+  Restaurant? get restaurant => _restaurant;
+
   Future<void> loadAllRestaurant() async {
     try {
       _restaurantList = await _databaseService.getAllItems();
+      _restaurant = null;
       _message = "berhasil memuat semua data";
       notifyListeners();
     } catch (e) {
       _message = "gagal memuat semua data";
       notifyListeners();
     }
+  }
+
+  Future<void> removeRestaurantById(String id) async {
+    try {
+      await _databaseService.removeItem(id);
+      _message = "berhasil menghapus data";
+      notifyListeners();
+    } catch (e) {
+      _message = "gagal menghapus data";
+      notifyListeners();
+    }
+  }
+
+  Future<void> saveRestaurant(Restaurant restaurant) async {
+    try {
+      final result = await _databaseService.insertItem(restaurant);
+      final isError = result == 0;
+      if (isError) {
+        _message = "gagal menyimpan data";
+      } else {
+        _message = "berhasil menyimpan data";
+      }
+      print(_message);
+      notifyListeners();
+    } catch (e) {
+      _message = "gagal menyimpan data";
+      notifyListeners();
+    }
+  }
+
+  bool checkItemBookmark(String id) {
+    return _restaurantList?.any((restaurant) => restaurant.id == id) ?? false;
   }
 }
